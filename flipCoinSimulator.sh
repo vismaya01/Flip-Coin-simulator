dbc569f20b0b3adac497aacf975525b25d05ccf3
vismaya01 <kvvismaya98@gmail.com>
2020-10-22 15:59:52 +0530

    Add: Coin flipped for 100 times and counted head & tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 echo "Welcome to flip coin simulator"
 
-#CONSTANT
-HEAD=0
+headCount=0
+tailCount=0
+count=1
 
-#TO PRINT THE HEAD AND TAIL
-flipCoin=$((RANDOM%2))
-if [ $flipCoin == $HEAD ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+#TO GET NUMBER OF TIMES HEAD AND TAIL WON
+while [ $count -le 100 ]
+do
+    flipCoin=$(( RANDOM%2 ))
+    if [ $flipCoin -eq 0 ]
+    then
+          ((headCount++))
+    else
+          ((tailCount++))
+    fi   
+
+    ((count++))
+done
+
+echo "Head has won $headCount times"
+echo "Tail has won $tailCount times"
