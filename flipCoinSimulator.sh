851892f15a795b53b5f7d16dace923a25330507b
vismaya01 <kvvismaya98@gmail.com>
2020-10-22 15:42:46 +0530

    Add: To check the flip coin head or tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "Welcome to flip coin simulator"
+
+#CONSTANT
+HEAD=0
+
+#TO PRINT THE HEAD AND TAIL
+flipCoin=$((RANDOM%2))
+if [ $flipCoin == $HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
