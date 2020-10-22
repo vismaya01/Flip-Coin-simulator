1e8f5c1629ee9ac3ae5f2955b56ec64b02a8326f
vismaya01 <kvvismaya98@gmail.com>
2020-10-22 16:38:56 +0530

    Modified: tie condition added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -32,4 +32,22 @@ then
 elif [ $hcount -eq $tcount ]
 then
 	echo "-Tie-"
+	while [ $(( hount - tcount )) -ne 2 ]
+          do
+		flipCoin=$((RANDOM%2))
+               if [ $flipCoin -eq 0 ]
+               then
+                    ((hcount++))
+               else
+                    ((tcount++))
+               fi
+         done
+	if [ $hcount -gt $tcount ]
+	then
+        	diff=$(($hcount - $tcount))
+        	echo "Heads Won by $diff || HeadCount : $hcount || TailCount : $tcount"
+	else
+        	diff=$(($tcount - $hcount))
+        	echo "Tails Won by $diff || TailCount : $tcount || HeadCount : $hcount"
+	fi
 fi
