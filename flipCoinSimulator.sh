2899217cbc343831817bc8b2663accb65f375ca7
vismaya01 <kvvismaya98@gmail.com>
2020-10-22 16:14:01 +0530

    Modified: To check the win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 echo "Welcome to flip coin simulator"
 
-headCount=0
-tailCount=0
-count=1
+hcount=0
+tcount=0
 
 #TO GET NUMBER OF TIMES HEAD AND TAIL WON
-while [ $count -le 100 ]
+while [ 1 ]
 do
-    flipCoin=$(( RANDOM%2 ))
-    if [ $flipCoin -eq 0 ]
-    then
-          ((headCount++))
-    else
-          ((tailCount++))
-    fi   
-
-    ((count++))
+	if [ $hcount -lt 21 -o $tcount -lt 21 ]
+	then
+		flipCoin=$((RANDOM%2))
+		if [ $flipCoin -eq 1 ]
+		then
+			tcount=$(($tcount+1))
+		else
+			hcount=$(($hcount+1))
+		fi
+	else
+		break
+	fi
 done
 
-echo "Head has won $headCount times"
-echo "Tail has won $tailCount times"
+if [ $hcount -gt $tcount ]
+then
+	diff=$(($hcount - $tcount))
+	echo "Heads Won by $diff || HeadCount : $hcount || TailCount : $tcount"
+elif [ $tcount -gt $hcount ]
+then
+	diff=$(($tcount - $hcount))
+	echo "Tails Won by $diff || TailCount : $tcount || HeadCount : $hcount"
+elif [ $hcount -eq $tcount ]
+then
+	echo "-Tie-"
+fi
